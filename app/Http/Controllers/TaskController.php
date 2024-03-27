fb10fa8030693f3dba4bddf554457f9771ada5f0
CLucasrodrigues22 <c.lucasrodrigues22@gmail.com>
2024-03-27 16:04:33 -0300

    [feat]: store tasks method complete

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,61 +5,56 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreTaskRequest;
 use App\Http\Requests\UpdateTaskRequest;
 use App\Models\Task;
+use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+
+    // Initialize objects when an instance of the class is created
+    protected $task;
+
+    public function __construct(Task $task)
+    {
+        $this->task = $task;
+    }
+
+    // Display a listing of the tasks.
+    public function index(Request $request)
     {
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
+    // Store a newly created task in storage
     public function store(StoreTaskRequest $request)
     {
-        //
+        // POST ["title", "description", "status"]
+        $task = Task::create([
+            "user_id" => auth()->user()->id,
+            "title" => $request->title,
+            "description" => $request->description,
+            "status" => $request->status
+        ]);
+
+        return response()->json([
+            "status" => true,
+            "data" => $task
+        ]);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Task $task)
+    // Display the specified task by id.
+    public function show()
+    {
+        // search by id
+    }
+
+    // Update the specified task in storage ["id", "title", "description", "status"]
+    public function update(UpdateTaskRequest $request)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Task $task)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateTaskRequest $request, Task $task)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Task $task)
+    // Remove the specified task from storage by id.
+    public function destroy()
     {
         //
     }
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -45,5 +44,11 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    // A user has one task
+    public function task()
+    {
+        return $this->hasOne('App\Models\Task');
     }
 }
